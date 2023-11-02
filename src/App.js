609e7057aa719662c69dbd29cc551c19df70d2da
Nikolai Matev <n.matev@hotmail.com>
2023-11-02 22:33:58 +0200

    Create product card module and small updates in app module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import ProductCard from './Product-Card';
+import React from 'react';
 
-function App() {
+function Img({ flag }) {
+  if (flag) {
+    return (
+      <img src={logo} className='App-logo' alt='logo' />
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi :)
-        </p>
-          Be happy :)
-      </header>
+    <div>
+      <img src={logo} className='App-logo' alt='logo' />
     </div>
-  );
+  )
+}
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      counter: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ // change counter only from setState
+        counter: this.state.counter + 1
+      })
+    }, 1000)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <Img flag={true} />
+          <ProductCard />
+          {this.state.counter}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
+export { Img }
